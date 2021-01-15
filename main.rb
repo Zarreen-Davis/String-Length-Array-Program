8eed7a0b0ac943ebc0191a3f95a68d4289619ec2
Zarreen Davis <zarreen.davis@outlook.com>
2021-01-15 00:51:53 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,8 @@
+#Write a Ruby program to create a new string where you check the string length and then convert it in to an array
+
+puts "Enter some text and I will tell you the length of it and then convert it into an array"
+text = gets.chomp
+puts text.length
+puts "\n"
+puts text.split
+
